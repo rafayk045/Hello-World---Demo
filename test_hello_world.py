ac4c88d00eb52f9294551f68e222bc0636b43cf9
rafayk045 <81592991+rafayk045@users.noreply.github.com>
2023-03-09 19:23:52 -0800

    clean yaml files and add hello world in pythn with unit test

---
+++ test_hello_world.py
@@ -0,0 +1,15 @@
+import unittest
+import sys
+from io import StringIO
+from hello_world import hello_world
+
+class TestHelloWorld(unittest.TestCase):
+    def test_hello_world(self):
+        captured_output = StringIO()
+        sys.stdout = captured_output
+        hello_world()
+        sys.stdout = sys.__stdout__
+        self.assertEqual(captured_output.getvalue().strip(), "Hello World!")
+
+if __name__ == '__main__':
+    unittest.main()
